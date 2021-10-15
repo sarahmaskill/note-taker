fe3893b2ced4bd7b5242049c960983f7ab1fa06f
Sarah Maskill <sarahnmaskill@gmail.com>
2021-10-14 21:26:36 -0400

    notes saving to db.json

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,69 @@
 const express = require('express');
 const path = require('path');
-const dbData = require('./db/db.json');
+const notes = require('./db/db.json');
+const fs = require('fs');
 
-const PORT = 3001;
+
+const PORT = process.env.port || 3001;
 
 const app = express();
+
+currentID = notes.length;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.use(express.static('public'));
 
-app.get('/api/db', (req, res) => res.json(dbData));
+// pull home page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
+//pull notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+//if page not found send to home
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+//API ROUTES
+app.get('/api/notes', (req, res) => res.json(notes));
+
+app.post("/api/notes", (req, res) => {
+    var newNote = req.body;
+    newNote["id"] = currentID +1;
+    currentID++;
+    console.log(newNote);
+    notes.push(newNote);
+    newNotes();
+    return res.status(200).end();
+});
+
+function newNotes() {
+    fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
+        if (err) {
+            console.log("error")
+            return console.log(err);
+        }
+        console.log("Success!");
+    });
+}
+
+// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
+    // Send a message to the client
+    res.json(`${req.method} request received to get notes`);
+  
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get notes`);
+  });
+  
 app.listen(PORT, () => {
   console.log(`Note Taker listening at http://localhost:${PORT}`);
 });
